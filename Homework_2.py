31747da0a796a7f214387b77248828db9e6e0731
MacAir <macair@MacBook-Air-MacAir.local>
2023-05-11 13:58:02 +0300

    Second task is added

---
+++ Homework_2.py
@@ -0,0 +1,6 @@
+# eleks_employees = ["Nataly", "John", "Peter", "James","Jacob"]
+# toshiba_employees = ["Nelly","Harry", "Jimmy","Natan", "Mark"]
+# new_toshiba_employees_list = (toshiba_employees + eleks_employees)
+# eleks_employees.clear()
+# print("Toshiba employees: " + str(new_toshiba_employees_list))
+# print("Elex employees: " + str(eleks_employees))
