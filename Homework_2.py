9def9d75a1db9cb24af035e16dca4760ac48470c
MacAir <macair@MacBook-Air-MacAir.local>
2023-05-11 14:03:38 +0300

    Third task is added

--- Homework_2.py
+++ Homework_2.py
@@ -4,3 +4,9 @@
 # eleks_employees.clear()
 # print("Toshiba employees: " + str(new_toshiba_employees_list))
 # print("Elex employees: " + str(eleks_employees))
+
+ # casino_blacklist = {"Nataly Parker", "John Smith", "Peter Prent", "James Jacob","Jacob Hammer"}
+# poker_blacklist = {"Nataly Parker", "Smith Janny", " Prent Harem", "James John","Hammer Henthon"}
+# alcohol_blacklist = {"Nataly Parker", "Perry Janny", " Oleh Harem", "Jerens John","Hammy Henthon"}
+# total_blacklist = casino_blacklist.intersection(poker_blacklist, alcohol_blacklist)
+# print(total_blacklist)
