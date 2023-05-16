7e5d58d1d89f7afffa7ab70b2413aebc65b0e25b
MacAir <macair@MacBook-Air-MacAir.local>
2023-05-16 17:13:04 +0300

    Fix errors

--- homework_2.py
+++ homework_2.py
@@ -3,9 +3,9 @@ toshiba_employees = ["Nelly","Harry", "Jimmy","Natan", "Mark"]
 new_toshiba_employees_list = (toshiba_employees + eleks_employees)
 eleks_employees.clear()
 print("Toshiba employees: " + str(new_toshiba_employees_list))
-print("Elex employees: " + str(eleks_employees))
+# print("Elex employees: " + str(eleks_employees))
 
- casino_blacklist = {"Nataly Parker", "John Smith", "Peter Prent", "James Jacob","Jacob Hammer"}
+casino_blacklist = {"Nataly Parker", "John Smith", "Peter Prent", "James Jacob","Jacob Hammer"}
 poker_blacklist = {"Nataly Parker", "Smith Janny", " Prent Harem", "James John","Hammer Henthon"}
 alcohol_blacklist = {"Nataly Parker", "Perry Janny", " Oleh Harem", "Jerens John","Hammy Henthon"}
 total_blacklist = casino_blacklist.intersection(poker_blacklist, alcohol_blacklist)
