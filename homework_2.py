f5da702ef447e1f03d025c137cfd86f268bf3410
MacAir <macair@MacBook-Air-MacAir.local>
2023-05-16 17:19:54 +0300

    Standarts PEP8

--- homework_2.py
+++ homework_2.py
@@ -1,13 +1,13 @@
-eleks_employees = ["Nataly", "John", "Peter", "James","Jacob"]
-toshiba_employees = ["Nelly","Harry", "Jimmy","Natan", "Mark"]
+eleks_employees = ["Nataly", "John", "Peter", "James", "Jacob"]
+toshiba_employees = ["Nelly", "Harry", "Jimmy", "Natan", "Mark"]
 new_toshiba_employees_list = (toshiba_employees + eleks_employees)
 eleks_employees.clear()
 print("Toshiba employees: " + str(new_toshiba_employees_list))
-# print("Elex employees: " + str(eleks_employees))
+print("Elex employees: " + str(eleks_employees))
 
-casino_blacklist = {"Nataly Parker", "John Smith", "Peter Prent", "James Jacob","Jacob Hammer"}
-poker_blacklist = {"Nataly Parker", "Smith Janny", " Prent Harem", "James John","Hammer Henthon"}
-alcohol_blacklist = {"Nataly Parker", "Perry Janny", " Oleh Harem", "Jerens John","Hammy Henthon"}
+casino_blacklist = {"Nataly Parker", "John Smith", "Peter Prent", "James Jacob", "Jacob Hammer"}
+poker_blacklist = {"Nataly Parker", "Smith Janny", " Prent Harem", "James John", "Hammer Henthon"}
+alcohol_blacklist = {"Nataly Parker", "Perry Janny", "Oleh Harem", "Jerens John", "Hammy Henthon"}
 total_blacklist = casino_blacklist.intersection(poker_blacklist, alcohol_blacklist)
 print(total_blacklist)
 
@@ -22,6 +22,6 @@ common_room.append([])
 print(vip_box)
 print(common_room)
 
-group_of_people = frozenset(["John Dow","Marta Dow", "John Dow"])
+group_of_people = frozenset(["John Dow", "Marta Dow", "John Dow"])
 print(group_of_people)
 
