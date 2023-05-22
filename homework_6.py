329a70bcf521516da7dc121487af618d45dc28f8
MacAir <macair@MacBook-Air-MacAir.local>
2023-05-22 21:12:32 +0300

    Homework_4

---
+++ homework_6.py
@@ -0,0 +1,20 @@
+# 1. Write an arithmetic function that takes 3 arguments: the first 2 are numbers,
+# and the third is the operation to be performed on them. If the third argument is +, add them up; if -,
+# then subtract; * - multiply; / - divide (first to second). Otherwise, return the string
+# Not known operation: {operation}. Describe the function in the attached file in such a way
+# that all checks in the __main__ task_1 block are performed correctly.
+# DO NOT CALL THE FUNCTION YOURSELF I HAVE ALREADY DONE THIS IN "assert" STATEMENTS
+
+# def third_operations(num_1, num_2, operation):
+#     if operation == "+":
+#         return num_1 + num_2
+#     elif operation == "-":
+#         return num_1 - num_2
+#     elif operation == "/":
+#         return num_1 / num_2
+#     elif operation == "*":
+#         return num_1 * num_2
+#     else:
+#         print(f"Not known operation: {operation}")
+
+
